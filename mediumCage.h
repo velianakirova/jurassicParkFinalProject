c684368f5c1a8f9fcc8a73d4956cf3c8213e3603
velianakirova <66217357+velianakirova@users.noreply.github.com>
2020-06-07 10:54:22 +0300

    Update mediumCage.h

--- mediumCage.h
+++ mediumCage.h
@@ -2,23 +2,11 @@
 #include "cage.h"
 
 class MediumCage : public Cage {
-private:
-	Dinosaur* dinosaur;
-	size_t count;
-	size_t capacity;
-	char* category;
-	char* era;
-	char* climate;
-	void copy(const MediumCage&);
-	void erase();
-
 public:
 	MediumCage(const char* climate);
 	MediumCage(const MediumCage&);
 	MediumCage& operator=(const MediumCage&);
 	~MediumCage();
-
-	bool isEqual(const char*, const char*);
 
 	string getCageType() const;
 	char* getClimate() const;
@@ -39,5 +27,17 @@ public:
 
 	void removeDinosaurFromCage(const char*);
 	void addNewDinosaur(const Dinosaur&);
+
+	bool isEqual(const char*, const char*);
 	void printInfo()const;
+private:
+	Dinosaur* dinosaur;
+	size_t count;
+	size_t capacity;
+	char* category;
+	char* era;
+	char* climate;
+	void copy(const MediumCage&);
+	void erase();
+
 };
